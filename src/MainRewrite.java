2ed67ae2b90950db87ae8252349ad16967a4291f
Dav Hunyah <xgarhontx@gmail.com>
2023-04-18 00:53:30 -0700

    only email and url is left

--- src/MainRewrite.java
+++ src/MainRewrite.java
@@ -5,10 +5,9 @@ import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MainRewrite {
-    public static int regex1SSN(String matcherString) { //TODO check consistent -'s or spaces?
-        String seperator = "[- | \\s]*";
-        Pattern pattern = Pattern.compile("^\\d{3}" + seperator + "\\d{2}" + seperator + "\\d{4}$");
+public class MainRewrite { //https://www.ssa.gov/history/ssn/geocard.html#:~:text=The%20nine%2Ddigit%20SSN%20is,digits%20is%20the%20Serial%20Number
+    public static int regex1SSN(String matcherString) { //(000-999)-(01-99)-(0001-9999)
+        Pattern pattern = Pattern.compile("^\\d{3}(?<separator>(-|\\s?))((?!01)\\d\\d)\\k<separator>(((?!0001))\\d\\d\\d\\d)$"); //neg lookahead https://www.regular-expressions.info/lookaround.html
         return doRegex(pattern.matcher(matcherString.trim()), matcherString);
     }
 
@@ -18,7 +17,10 @@ public class MainRewrite {
     }
 
     public static int regex3Email(String matcherString) { //TODO https://knowledge.validity.com/hc/en-us/articles/220560587-What-are-the-rules-for-email-address-syntax-
-        Pattern pattern = Pattern.compile("^[\\S]{1,}[@][\\S]{1,}[.][\\S]{1,}$");
+        String name = "(?!.[^a-z0-9]).?(?!.+[^a-z0-9]{2,}).+(?!.[^a-z0-9]).?";
+        String domain = "[a-z0-9]+.";
+        String topDomain = "[a-z0-9.\\.]+(?!\\.)[a-z0-9]";
+        Pattern pattern = Pattern.compile("^" + name + "@" + domain + topDomain + "$", Pattern.CASE_INSENSITIVE);
         return doRegex(pattern.matcher(matcherString.trim()), matcherString);
     }
 
@@ -33,29 +35,43 @@ public class MainRewrite {
     public static int regex5Date(String matcherString) { //TODO helper to valid date
         String month = "(?<month>0[1-9]|1[12]|[1-9])";
         String separate = "(?<separator>[-|/])";
-        String day = "(?<day>0[1-9]|[12][0-9]|[3[01]]|[1-9])";
+        String day = "(?<day>0[1-9]|[12][0-9]|3[01]|[1-9])";
         String separate1 = "\\k<separator>";
         String year = "(?<year>[0-9]+)";
         Pattern pattern = Pattern.compile("^" + month + separate + day + separate1 + year + "$"); //MM-DD-YYYY
+        //Validate Date
         Matcher matcher = pattern.matcher(matcherString.trim());
         if (doRegex(matcher, matcherString) == 1) {
-            DateTimeFormatter df = DateTimeFormatter.ofPattern("M-d-y", Locale.US);
-//            SimpleDateFormat df = new SimpleDateFormat("M-d-y");
-//            df.setLenient(false);
-            try {
-                df.parse(matcherString.replaceAll("/", "-"), new ParsePosition(0));
-                return 1;
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-                return 0;
-            }
+            return validateDate(
+                    Integer.parseInt(matcher.group("month")),
+                    Integer.parseInt(matcher.group("day")),
+                    Integer.parseInt(matcher.group("year"))
+            );
         }
         return 0;
     }
 
+    private static int validateDate(int month, int day, int year) { //https://www.scaler.com/topics/date-validation-in-javascript/
+        int[] ListofDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (month == 1 || month > 2) {
+            if (day > ListofDays[month - 1]) {
+                return 0;
+            }
+        } else if (month == 2) {
+            boolean leapYear = false;
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) leapYear = true;
+            if (!leapYear && (day >= 29)) return 0;
+            else
+            if (leapYear && (day > 29)) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
     public static int regex6HouseAddress(String matcherString) {
-        String houseNumber = "[0-9]+"; //street number
-        String streetName = "[A-Za-z0-9..\\s]+";
+        String houseNumber = "[0-9]+\\s+"; //street number
+        String streetName = "[A-Za-z0-9..\\s]+\\s+";
         String streetAbb = "(street|st[.]?|road|rd[.]?|boulevard|blvd[.]?|avenue|ave[.]?)";
         Pattern pattern = Pattern.compile("^" + houseNumber + streetName + streetAbb + "$", Pattern.CASE_INSENSITIVE);
         return doRegex(pattern.matcher(matcherString.trim()), matcherString);
@@ -67,8 +83,13 @@ public class MainRewrite {
 //        String state = "([A-Za-z][A-Za-z])";
         String state = "(AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY)";
         String spacing = "[\\s]*";
-        String zip = "[0-9]{5}([-][0-9]+)?";
+        String zip = "[0-9]{5}([-][0-9]{4})?";
         Pattern pattern = Pattern.compile("^" + city + comma + state + spacing + zip + "$");
+        return doRegex(pattern.matcher(matcherString.trim()), matcherString);
+    }
+
+    public static int regex8MilTime(String matcherString) {
+        Pattern pattern = Pattern.compile("^([01][0-9]|2[0-3])([0-5][0-9])$");
         return doRegex(pattern.matcher(matcherString.trim()), matcherString);
     }
 
@@ -77,12 +98,15 @@ public class MainRewrite {
         return doRegex(pattern.matcher(matcherString.trim()), matcherString);
     }
 
-    public static int regex10URL(String matcherString) {
-        String protocol = "(http[s]?://)?";
-//        String domain = "[A-Za-z0-9..@:]*/";
+    public static int regex10URL(String matcherString) { //(?(?=regex)then|else)
+        String protocol = "((?=.*{4}http)[s]?://)|(?!.\\.)(?=\\.*).+";
         String domain = ".*";
-//        String path = "[A-Za-z0-9..@:]*/";
         Pattern pattern = Pattern.compile("^" + protocol + domain + "$", Pattern.CASE_INSENSITIVE);
+        return doRegex(pattern.matcher(matcherString.trim()), matcherString);
+    }
+
+    public static int regex11Password(String matcherString) {
+        Pattern pattern = Pattern.compile("^(?=.*[A-Z]+)(?=.*[a-z]+)(?=.*[^A-Za-z0-9]+)(?=.*[a-z]+)(?=.*\\d+)(?!.*[a-z]{4,}).{10,}$");
         return doRegex(pattern.matcher(matcherString.trim()), matcherString);
     }
 
@@ -93,11 +117,16 @@ public class MainRewrite {
 
     public static int doRegex(Matcher matcher, String matcherString) {
         int found = 0;
-        while (matcher.find()) {
+        if (matcher.find()) {
             System.out.printf("\"%s\": Found match \"%s\" from index %d to %d.\n",
                     matcherString, matcher.group(), matcher.start(), matcher.end());
             found++;
         }
+//        while (matcher.find()) {
+//            System.out.printf("\"%s\": Found match \"%s\" from index %d to %d.\n",
+//                    matcherString, matcher.group(), matcher.start(), matcher.end());
+//            found++;
+//        }
         if (found == 0) {
             System.out.printf("\"%s\": No match found.\n", matcherString);
         }
--- test/MainRewriteTest.java
+++ test/MainRewriteTest.java
@@ -11,12 +11,19 @@ class MainRewriteTest {
         assertEquals(1, MainRewrite.regex1SSN("123121234"));
         assertEquals(1, MainRewrite.regex1SSN("123 12 1234"));
         assertEquals(1, MainRewrite.regex1SSN("          123-12-1234"));
+        assertEquals(0, MainRewrite.regex1SSN("12D-12-1234"));
+        assertEquals(0, MainRewrite.regex1SSN("123-1D-1234"));
+        assertEquals(0, MainRewrite.regex1SSN("123-12-12a4"));
         assertEquals(0, MainRewrite.regex1SSN("1234-12-1234"));
         assertEquals(0, MainRewrite.regex1SSN("123-123-1234"));
         assertEquals(0, MainRewrite.regex1SSN("123-12-12345"));
         assertEquals(0, MainRewrite.regex1SSN("1234121234"));
         assertEquals(0, MainRewrite.regex1SSN("1234 123 12345"));
         assertEquals(0, MainRewrite.regex1SSN("1 234-12-1234"));
+        assertEquals(0, MainRewrite.regex1SSN("123-121234"));
+        assertEquals(0, MainRewrite.regex1SSN("12312-1234"));
+        assertEquals(0, MainRewrite.regex1SSN("123-01-1234"));
+        assertEquals(0, MainRewrite.regex1SSN("123-10-0001"));
     }
 
     @Test
@@ -33,10 +40,18 @@ class MainRewriteTest {
     void regex3Email() {
         System.out.println("\nregex3Email" + "------------------------------");
         assertEquals(1, MainRewrite.regex3Email("bruh@gmail.com"));
+        assertEquals(1, MainRewrite.regex3Email("bruh@gmail.com.gov.co"));
+        assertEquals(0, MainRewrite.regex3Email("bruh@gmail.com.gov.co."));
+        assertEquals(1, MainRewrite.regex3Email("b@gmail.com"));
         assertEquals(0, MainRewrite.regex3Email("bruh@gmail."));
         assertEquals(0, MainRewrite.regex3Email("@gmail.com"));
         assertEquals(0, MainRewrite.regex3Email("bruhgmail.com"));
         assertEquals(0, MainRewrite.regex3Email("bruh@.com"));
+
+        assertEquals(0, MainRewrite.regex3Email("&@gmail.com"));
+        assertEquals(0, MainRewrite.regex3Email("!bruh@gmail.com"));
+        assertEquals(0, MainRewrite.regex3Email("bruh!@gmail.com"));
+        assertEquals(0, MainRewrite.regex3Email("br!!uh@gmail.com"));
     }
 
     @Test
@@ -50,6 +65,9 @@ class MainRewriteTest {
         assertEquals(1, MainRewrite.regex4NameOnRoster("Hills, De'Ante, B"));
         assertEquals(0, MainRewrite.regex4NameOnRoster("Huynh, Gia-Bao (David), Huu"));
         assertEquals(0, MainRewrite.regex4NameOnRoster("Last, First, Middle"));
+        assertEquals(0, MainRewrite.regex4NameOnRoster("Last First M"));
+        assertEquals(0, MainRewrite.regex4NameOnRoster("Last, First M"));
+        assertEquals(0, MainRewrite.regex4NameOnRoster("Last First, M"));
     }
 
     @Test
@@ -60,6 +78,8 @@ class MainRewriteTest {
         assertEquals(1, MainRewrite.regex5Date("1-1-2023"));
         assertEquals(1, MainRewrite.regex5Date("1/1/2023"));
         assertEquals(0, MainRewrite.regex5Date("02/29/2001"));
+        assertEquals(0, MainRewrite.regex5Date("04/31/2001"));
+        assertEquals(1, MainRewrite.regex5Date("4/30/2001"));
         assertEquals(0, MainRewrite.regex5Date("13/1/2023"));
         assertEquals(0, MainRewrite.regex5Date("12/32/2023"));
         assertEquals(0, MainRewrite.regex5Date("99/99/2023"));
@@ -79,19 +99,42 @@ class MainRewriteTest {
         assertEquals(1, MainRewrite.regex6HouseAddress("84 South Rock Maple Ave."));
         assertEquals(1, MainRewrite.regex6HouseAddress("84 South Rock Maple Avenue"));
         assertEquals(0, MainRewrite.regex6HouseAddress("84 South Rock Maple Aven."));
+        assertEquals(0, MainRewrite.regex6HouseAddress("South Rock Maple Ave"));
+        assertEquals(0, MainRewrite.regex6HouseAddress("2134 E Cool 21St"));
+        assertEquals(0, MainRewrite.regex6HouseAddress("345 road"));
+        assertEquals(0, MainRewrite.regex6HouseAddress("345road st"));
     }
 
     @Test
     void regex7CityState() {
         System.out.println("\nregex7CityState" + "------------------------------");
         assertEquals(1, MainRewrite.regex7CityState("Tacoma, WA 12345"));
-        assertEquals(1, MainRewrite.regex7CityState("Tacoma, WA 12345-1234"));
-        assertEquals(1, MainRewrite.regex7CityState("Tacoma,WA 12345-1234"));
+        assertEquals(1, MainRewrite.regex7CityState("Tacoma, WA 12345"));
+        assertEquals(1, MainRewrite.regex7CityState("NYC, NY 12345-1234"));
+        assertEquals(1, MainRewrite.regex7CityState("Tacoma, WA 98402"));
+        assertEquals(1, MainRewrite.regex7CityState("Seattle, CA 71123"));
         assertEquals(0, MainRewrite.regex7CityState("Tacoma,WA 12345-"));
         assertEquals(0, MainRewrite.regex7CityState("Tacoma, WA 1234"));
+        assertEquals(0, MainRewrite.regex7CityState("Tacoma, WA 123456"));
         assertEquals(0, MainRewrite.regex7CityState("Tacoma WA 12345"));
         assertEquals(0, MainRewrite.regex7CityState("Tacoma, WA 1234-1234"));
+        assertEquals(0, MainRewrite.regex7CityState("Tacoma, WA 12345-12345"));
         assertEquals(0, MainRewrite.regex7CityState("Tacoma, WZ 12345"));
+    }
+
+    @Test
+    void regex8MilTime() {
+        System.out.println("\nregex8MilTime" + "------------------------------");
+        assertEquals(1, MainRewrite.regex8MilTime("0000"));
+        assertEquals(1, MainRewrite.regex8MilTime("0500"));
+        assertEquals(1, MainRewrite.regex8MilTime("1000"));
+        assertEquals(1, MainRewrite.regex8MilTime("1500"));
+        assertEquals(1, MainRewrite.regex8MilTime("2359"));
+        assertEquals(0, MainRewrite.regex8MilTime("500"));
+        assertEquals(0, MainRewrite.regex8MilTime("0060"));
+        assertEquals(0, MainRewrite.regex8MilTime("2400"));
+        assertEquals(0, MainRewrite.regex8MilTime("12450"));
+        assertEquals(0, MainRewrite.regex8MilTime("17 30"));
     }
 
     @Test
@@ -119,11 +162,38 @@ class MainRewriteTest {
     }
 
     @Test
+    void regex11Password() {
+        System.out.println("\nregex11Password" + "------------------------------");
+        assertEquals(1, MainRewrite.regex11Password("Wasd123456!"));
+        assertEquals(1, MainRewrite.regex11Password("I'mSupeRC00l"));
+        assertEquals(1, MainRewrite.regex11Password("AmaZinGCoolPass018287*!,"));
+        assertEquals(1, MainRewrite.regex11Password("IAm.TheM1n"));
+        assertEquals(1, MainRewrite.regex11Password("IAm.TheMAXIMUMMMMMMMMMMWMWMWMmmmWW12345as"));
+        assertEquals(0, MainRewrite.regex11Password("I1mSupeRC00l"));
+        assertEquals(0, MainRewrite.regex11Password("I'mSupeRcool"));
+        assertEquals(0, MainRewrite.regex11Password("IAm.TheM1n2343 sdfg345 346 rtrgf d56"));
+        assertEquals(0, MainRewrite.regex11Password("IAm.TheM1"));
+        assertEquals(0, MainRewrite.regex11Password("wasd123456!"));
+        assertEquals(0, MainRewrite.regex11Password("Wasdwasdwasd123456!"));
+        assertEquals(0, MainRewrite.regex11Password("1"));
+        assertEquals(0, MainRewrite.regex11Password("www.google.com"));
+    }
+
+    @Test
     void regex10URL() {
         System.out.println("\nregex10URL" + "------------------------------");
         assertEquals(1, MainRewrite.regex10URL("https://www.google.com"));
         assertEquals(1, MainRewrite.regex10URL("www.google.com"));
+        assertEquals(1, MainRewrite.regex10URL("https://www.regular-expressions.info/quickstart.html"));
+        assertEquals(1, MainRewrite.regex10URL("https://docs.oracle.com/javase/tutorial/essential/regex/bounds.html"));
+        assertEquals(1, MainRewrite.regex10URL("https://canvas.uw.edu/courses/1642545/assignments/8223051"));
+        assertEquals(0, MainRewrite.regex10URL("https//canvas.uw.edu/courses/1642545/assignments/8223051"));
+        assertEquals(0, MainRewrite.regex10URL("https:/canvas.uw.edu/courses/1642545/assignments/8223051"));
+        assertEquals(0, MainRewrite.regex10URL("https:canvas.uw.edu/courses/1642545/assignments/8223051"));
+        assertEquals(0, MainRewrite.regex10URL("https://wwwgooglecom"));
+        assertEquals(0, MainRewrite.regex10URL("wwwgooglecom"));
     }
+
 
     @Test
     void regex12IonOdd() {
@@ -131,8 +201,12 @@ class MainRewriteTest {
         assertEquals(1, MainRewrite.regex12IonOdd("ion"));
         assertEquals(1, MainRewrite.regex12IonOdd("quion"));
         assertEquals(1, MainRewrite.regex12IonOdd("million"));
+        assertEquals(1, MainRewrite.regex12IonOdd("bruhion"));
+        assertEquals(1, MainRewrite.regex12IonOdd("wWOWOWOowwion"));
         assertEquals(0, MainRewrite.regex12IonOdd("Trillion"));
         assertEquals(0, MainRewrite.regex12IonOdd("POTION"));
         assertEquals(0, MainRewrite.regex12IonOdd("lion"));
+        assertEquals(0, MainRewrite.regex12IonOdd("ow ion"));
+        assertEquals(0, MainRewrite.regex12IonOdd("ioningy"));
     }
 }
